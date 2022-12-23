d9ef5f8784daa38a623784b2815a5b4b0790873e
ted9219 <ted9219@gmail.com>
2022-12-23 16:17:46 +0900

    query tuning for speed

--- inst/sql/sql_server/CKim_Abdominal_pain.sql
+++ inst/sql/sql_server/CKim_Abdominal_pain.sql
@@ -18,6 +18,11 @@ UNION  select c.concept_id
 ) C
 ;
 
+ SELECT co.* into #CodeSetData_1
+ FROM @cdm_database_schema.condition_occurrence_covid19 co
+ JOIN #Codesets codesets on ((co.condition_concept_id = codesets.concept_id and codesets.codeset_id = 1));
+
+
 with primary_events (event_id, person_id, start_date, end_date, op_start_date, op_end_date, visit_occurrence_id) as
 (
 -- Begin Primary Events
@@ -32,12 +37,7 @@ FROM
   -- Begin Condition Occurrence Criteria
 SELECT C.person_id, C.condition_occurrence_id as event_id, C.condition_start_date as start_date, COALESCE(C.condition_end_date, DATEADD(day,1,C.condition_start_date)) as end_date,
   C.visit_occurrence_id, C.condition_start_date as sort_date
-FROM 
-(
-  SELECT co.* 
-  FROM @cdm_database_schema.CONDITION_OCCURRENCE co
-  JOIN #Codesets codesets on ((co.condition_concept_id = codesets.concept_id and codesets.codeset_id = 1))
-) C
+FROM #CodeSetData_1 C
 
 
 -- End Condition Occurrence Criteria
@@ -327,3 +327,6 @@ DROP TABLE #included_events;
 
 TRUNCATE TABLE #Codesets;
 DROP TABLE #Codesets;
+
+TRUNCATE TABLE #CodeSetData_1;
+DROP TABLE #CodeSetData_1;
